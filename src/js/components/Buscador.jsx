4a5a7f52c76927c4507d89dbc8313799453bc18b
andlicon <andresjzabalac@hotmail.com>
2023-04-27 10:53:30 -0400

    ADD: nueva identacion a los documentos

---
+++ src/js/components/Buscador.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Buscador = props => {
+    return (
+        <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <a href="/">Buscar</a>
+        </form>
+    );
+};
+
+export default Buscador;
