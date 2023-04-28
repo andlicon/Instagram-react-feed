54682a03406da1780367896cdca8ed523e9668ef
andlicon <andresjzabalac@hotmail.com>
2023-04-28 09:07:57 -0400

    RM: se removieron importaciones que no se usaban

--- src/js/pages/Home.jsx
+++ src/js/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 // componentes
 import NavBarBuscar from '../components/NavBarBuscar.jsx';
-import CloseButton from '../components/CloseButton.jsx';
 
 const Home = () => {
   return (
