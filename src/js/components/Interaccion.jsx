3b0de3797e9eb5d23e7f3146296727a5e29e0694
andlicon <andresjzabalac@hotmail.com>
2023-04-28 10:13:26 -0400

    ADD: estilo a los botones de interaccion

--- src/js/components/Interaccion.jsx
+++ src/js/components/Interaccion.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import '../../styles/button.css'
+import '../../styles/Interaccion.css';
 
 const Interaccion = props => {
-    return(
-        <button className="icon-button">
-            <span><i className={props.icono}></i></span>
-        </button>
-    );
+  return (
+    <button
+      className="icon-button interaccion-boton"
+      onClick={(e) => { e.preventDefault() }}>
+      <span><i className={`interaccion ${props.icono}`}></i></span>
+    </button>
+  );
 };
 Interaccion.propTypes = {
-    icono: PropTypes.string
+  icono: PropTypes.string
 };
 
 export default Interaccion;
