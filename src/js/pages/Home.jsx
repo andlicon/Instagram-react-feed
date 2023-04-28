64fbdaeba9be36989489f76266710892b2f9af2f
andlicon <andresjzabalac@hotmail.com>
2023-04-28 11:58:12 -0400

    ADD: un contenedor home

--- src/js/pages/Home.jsx
+++ src/js/pages/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 // componentes
 import NavBarBuscar from '../components/NavBarBuscar.jsx';
 import NavBarInteraccion from '../components/NavBarPrincipal.jsx'
+import '../../styles/Home.css';
 
 const Home = () => {
   return (
-    <>
+    <div className="home">
       <NavBarBuscar />
       <NavBarInteraccion />
-    </>
+    </div>
   );
 };
 
