1c40b52fe7ad1857f6a236381a6d57416a55cc86
andlicon <andresjzabalac@hotmail.com>
2023-05-03 12:43:23 -0400

    ADD: estilos a las historias

--- src/js/components/Historia.jsx
+++ src/js/components/Historia.jsx
@@ -1,17 +1,22 @@
 import React, { useEffect } from 'react';
 
+import '../../styles/historia.css';
+
 const Historia = ({ username, fotoPerfil }) => {
 
     return (
-        <li className='historia'>    
-            <img 
-                src={fotoPerfil} 
-                alt={`foto de perfil del usuario ${username}`} 
-                className='historia-img'
-            />
-            <p className='historia-username'>
-                {username}
-            </p>
+        <li className='historia'>
+            <a className="historia-a"
+                href="/">
+                <img
+                    src={fotoPerfil}
+                    alt={`foto de perfil del usuario ${username}`}
+                    className='historia-img'
+                />
+                <p className='historia-username'>
+                    {username}
+                </p>
+            </a>
         </li>
     );
 };
