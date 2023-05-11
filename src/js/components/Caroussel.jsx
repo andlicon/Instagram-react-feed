09f9a428fdeef93b858608f9e028493a89a19f72
andlicon <andresjzabalac@hotmail.com>
2023-05-11 07:27:30 -0400

    ADD: se anadio la funcion asincrona para traer las historias

--- src/js/components/Caroussel.jsx
+++ src/js/components/Caroussel.jsx
@@ -5,42 +5,42 @@ import Historia from './Historia.jsx';
 import '../../styles/caroussel.css';
 
 const Caroussel = (props) => {
-    const [historias, setHistorias] = useState([]);
-    const [cantidadHistorias, setCantidadHistorias]  = useState(20);
+  const [historias, setHistorias] = useState([]);
+  const [cantidadHistorias, setCantidadHistorias] = useState(20);
 
-    useEffect( () => {
-        obtenerHistorias();
-    }, []);
+  useEffect(() => {
+    obtenerHistorias();
+  }, []);
 
-    const obtenerHistorias = async () => {
-        fetch(`https://randomuser.me/api/?results=${cantidadHistorias}`)
-        .then(response => response.json())
-        .then(data => setHistorias(data.results))
-    }
+  const obtenerHistorias = async () => {
+    let response = await fetch(`https://randomuser.me/api/?results=${cantidadHistorias}`)
+    let data = await response.json();
+    setHistorias(data.results);
+  }
 
-    return (
-        <ul className='caroussel-historias'>
-            <span className='caroussel-pasar' id='carousselPrev'>
-                <i className="bi bi-arrow-left-short"></i>
-            </span>
-            {
-                historias.map( (elemento, index) => {
-                    const username = elemento.login.username;
-                    const fotoPerfil = elemento.picture.thumbnail;
+  return (
+    <ul className='caroussel-historias'>
+      <span className='caroussel-pasar' id='carousselPrev'>
+        <i className="bi bi-arrow-left-short"></i>
+      </span>
+      {
+        historias.map((elemento, index) => {
+          const username = elemento.login.username;
+          const fotoPerfil = elemento.picture.thumbnail;
 
-                    return(
-                        <Historia 
-                            key = {index} 
-                            username = {username}
-                            fotoPerfil = {fotoPerfil}
-                        />
-                    );
-                })
-            }
-            <span className='caroussel-pasar' id='carousselNext'>
-                <i className="bi bi-arrow-right-short"></i>
-            </span>
-        </ul>
-    );
+          return (
+            <Historia
+              key={index}
+              username={username}
+              fotoPerfil={fotoPerfil}
+            />
+          );
+        })
+      }
+      <span className='caroussel-pasar' id='carousselNext'>
+        <i className="bi bi-arrow-right-short"></i>
+      </span>
+    </ul>
+  );
 };
 export default Caroussel;
