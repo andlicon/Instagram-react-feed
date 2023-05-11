46e14fec8b2f0f5cfab49e3bb3c1b4edae8baafb
andlicon <andresjzabalac@hotmail.com>
2023-05-11 15:11:00 -0400

    ADD: contenido ahora tiene botones para pasar entre fotos

--- src/js/components/Contenido.jsx
+++ src/js/components/Contenido.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 import '../../styles/contenido.css'
 
 const Contenido = ({ contenidos }) => {
+  const isPasable = (contenidos.length > 1);
+
   return (
     <div className='contenidos'>
+      {
+        isPasable&&
+          <button 
+            className='contenidos__pasador contenidos__pasador--prev'>
+            <i className='bi bi-caret-left'></i>
+          </button>
+      }
       {
         contenidos.map( (contenido, index) => {
           return(
@@ -14,6 +23,12 @@ const Contenido = ({ contenidos }) => {
               className='contenidos__img' />
           )
         })
+      }
+      {
+        isPasable&&
+          <button className='contenidos__pasador contenidos__pasador--next'>
+            <i className='bi bi-caret-right'></i>
+          </button>
       }
     </div>
   );
