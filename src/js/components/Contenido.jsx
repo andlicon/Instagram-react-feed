14e81b074c1bbe8cf44c4144679dfa99060042ad
andlicon <andresjzabalac@hotmail.com>
2023-05-12 08:11:52 -0400

    ADD: ahora el contenido se puede ir pasando

--- src/js/components/Contenido.jsx
+++ src/js/components/Contenido.jsx
@@ -1,34 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/contenido.css'
 
 const Contenido = ({ contenidos }) => {
-  const isPasable = (contenidos.length > 1);
+  const cantidad = (contenidos.length);
+  const isPasable = (cantidad > 1);
+  // hooks
+  const [mostrar, setMostrar] = useState(0);
+
+  const contenidoSiguiente = (event, direccion) => {
+    const NEXT = true;
+    const PREV = false;
+
+    if (direccion == NEXT
+        && (mostrar+1) < cantidad) {
+      setMostrar(mostrar + 1);
+    }
+    if (direccion == PREV 
+        && (mostrar-1) >= 0) {
+      setMostrar(mostrar - 1);
+    }
+  }
 
   return (
     <div className='contenidos'>
       {
-        isPasable&&
-          <button 
-            className='contenidos__pasador contenidos__pasador--prev'>
-            <i className='bi bi-caret-left'></i>
-          </button>
+        isPasable &&
+        <button className='contenidos__pasador contenidos__pasador--prev'
+          onClick={event => contenidoSiguiente(event, false)}>
+          <i className='bi bi-caret-left'></i>
+        </button>
       }
       {
-        contenidos.map( (contenido, index) => {
-          return(
-            <img 
-              key={ index }
-              src='' 
+        contenidos.map((contenido, index) => {
+          return (
+            <img
+              key={index}
+              src=''
               alt=''
               className='contenidos__img' />
           )
         })
       }
       {
-        isPasable&&
-          <button className='contenidos__pasador contenidos__pasador--next'>
-            <i className='bi bi-caret-right'></i>
-          </button>
+        isPasable &&
+        <div className='contenidos__status'>
+          {
+            contenidos.map((e, index) => {
+              return (
+                <span
+                  key={index}
+                  className={`status ${index == mostrar && 'status--actual'}`}>
+
+                </span>
+              )
+            })
+          }
+        </div>
+      }
+      {
+        isPasable &&
+        <button className='contenidos__pasador contenidos__pasador--next'
+          onClick={event => contenidoSiguiente(event, true)}>
+          <i className='bi bi-caret-right'></i>
+        </button>
       }
     </div>
   );
